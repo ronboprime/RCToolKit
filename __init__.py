4d313b930a65483a6cccbc6aeca4e26a93cf2846
ronboprime <ronboprime@gmail.com>
2023-10-14 22:18:52 -0700

    fuck this is annoying

--- __init__.py
+++ __init__.py
@@ -6,18 +6,20 @@ bl_info = {
     "email":"ronboprime@gmail.com",
     "version": (1, 3),
     "blender": (4, 0, 0),
-    "location": "Node Editor> Navigation Bar> Tool",
+    "location": "Node Editor> Navigation Bar> RCToolKit",
     "warning": "relax! You'll never make it out, alive!", 
     "doc_url": "https://www.ronboprime.com/current/rct/docs",
     "tracker_url": "",
     "support": "COMMUNITY",
     "category": "Node",
 }
-import bpy
 
-nodes=["Thouies","Hundoes","Tenths","Half/Double","Third/3x","Quarter/4x","Fifth/5x","Eighth/8x","Smoothing","Puppet","Camera Mockup","Broken At Ranges","Curve To Mesh","QuickInvert","QuickStartIco","QuickStartCube","QuickStartCone","QuickStartCurve","QuickStartTorus","QuickStartGrid"]
+nodes=["Thouies","Hundoes","Tenths","Smoothing","Half/Double","Third/3x","Quarter/4x","Fifth/5x","Eighth/8x","Puppet","Camera Mockup","Broken At Ranges","Curve To Mesh","QuickInvert","QuickStartIco","QuickStartCube","QuickStartCone","QuickStartCurve","QuickStartTorus","QuickStartGrid"]
 icons=["EMPTY_ARROWS","EMPTY_ARROWS","EMPTY_ARROWS","LINENUMBERS_ON","PREV_KEYFRAME","FF","NEXT_KEYFRAME","REW","SELECT_DIFFERENCE","IPO_BEZIER","SYSTEM","GP_SELECT_POINTS","SEQ_CHROMA_SCOPE","OUTLINER_DATA_CAMERA","PROP_PROJECTED","ORIENTATION_GIMBAL","MOD_PARTICLE_INSTANCE","MESH_CUBE","PROP_OFF","PROP_ON","MESH_GRID"]
+
 data ={node:icon for (node,icon) in zip(nodes,icons)}
+
+node_properties=bpy.context.scene.node_properties
 
 class NodeProperties(bpy.types.PropertyGroup):
     thingo:bpy.props.StringProperty(
@@ -25,14 +27,15 @@ class NodeProperties(bpy.types.PropertyGroup):
     default="tiddy",
     description="tuesday"
     )
+
 class NODE_OT_RCToolKitOperator(bpy.types.Operator):
     bl_idname='node.rctoperator'
     bl_label='RCToolKit Operator'
 
     CNode:bpy.props.StringProperty(
-        name="Custom Node",
+        name="Custom Node Name",
         description="For Swapping properties",
-        default="RCT-Thouies"
+        default="Thouies"
         )
     INode:bpy.props.StringProperty(
         name="Icon for menu",
@@ -49,11 +52,10 @@ class NODE_OT_RCToolKitOperator(bpy.types.Operator):
         newnode = context.active_node
         newnode.location.x -= 400
         return {'FINISHED'}
-    
 
 class NODE_PT_RCToolKitPanel(bpy.types.Panel):
     bl_idname="NODE_PT_rctpanel"
-    bl_label="RCToolKit Panel"
+    bl_label=" -= RCToolKit =- "
     bl_space_type='NODE_EDITOR'
     bl_region_type='UI'
     bl_category='Tool'
@@ -61,8 +63,7 @@ class NODE_PT_RCToolKitPanel(bpy.types.Panel):
     def draw(self,context):
         col=self.layout.column(align=True)
         doodle(col)
-        
-        
+
 def doodle(col):
    for k,v in data.items():
      templ8="RCT-"+str(k)
@@ -72,16 +73,17 @@ def doodle(col):
 
 classes = [NodeProperties,NODE_OT_RCToolKitOperator,NODE_PT_RCToolKitPanel]
 
-def thing():
-    node_properties=bpy.context.scene.node_properties
-
 def register():
     for cls in classes:
         bpy.utils.register_class(cls)
+        print(str(cls)+" Loaded")
     bpy.types.Scene.node_properties=bpy.props.PointerProperty(type=NodeProperties)        
+    print("Properties assigned.")    
+    print("RCToolKit Loaded")
 
 def unregister():
     for cls in classes:
         bpy.utils.unregister_class(cls)
-        
+    print("Goodbye, cruel world!")    
+
 register()
