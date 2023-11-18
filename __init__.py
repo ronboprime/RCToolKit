3e9c24d7d1a048cd16d8401f32999c541d8ae483
ronboprime <64212700+ronboprime@users.noreply.github.com>
2023-11-18 05:21:02 -0800

    Update __init__.py
    BEST VERSION YET
    
    Signed-off-by: ronboprime <64212700+ronboprime@users.noreply.github.com>

--- __init__.py
+++ __init__.py
@@ -1,49 +1,90 @@
 import bpy
 
-class NODE_OT_add_node(bpy.types.Operator):
-    bl_idname = "node.add_node"
-    bl_label = "Add Node"
-    
-    # Property to hold the name of the node to add
-    node_name: bpy.props.StringProperty()
+bl_info = {
+    "name": "Display Local Nodes",
+    "description": "Adds a display in your Node Editor, to show local nodes, in the Tool section.",
+    "author": "Ronboprime",
+    "email":"ronboprime@gmail.com",
+    "version": (1, 5),
+    "blender": (4,0,0),
+    "location": "Node Editor> Navigation Bar> Tool",
+    "warning": "", 
+    "doc_url": "",
+    "tracker_url": "",
+    "category": "Node",
+}
 
-    @classmethod
-    def poll(cls, context):
-        return context.space_data.tree_type == 'GeometryNodeTree'
+# Get all node groups
+#node_tree = bpy.context.space_data.node_tree
+
+class NODE_OT_add_node_group(bpy.types.Operator):
+    bl_idname = "node_ot.add_node_group"
+    bl_label = "Add Node Group"
+    
+    # Property to hold the name of the node group to add
+    node_group_name: bpy.props.StringProperty()
 
     def execute(self, context):
-        # Get the active node tree
+    #sanity check
+        obj=bpy.context.active_object
+        print(obj)
+
+    #is this thing on?
+        if obj==None:
+            self.report({'WARNING'},"how can you have any node tree, if you don't have your active object!")
+            return{"CANCELLED"}
+
+    # Get the node groups
+        node_groups = bpy.data.node_groups
+
+    # Get the active node tree
         node_tree = context.space_data.node_tree
 
-        # Create a new node
-        new_node = node_tree.nodes.new(type=self.node_name)
+    # what if there's no node tree?        
+        try:
+    # Create a new group node
+            new_node = node_tree.nodes.new(type='GeometryNodeGroup')
 
-        # Position the new node
-        new_node.location = (0, 0)
+    # Set the node group of the new node
+            new_node.node_tree = bpy.data.node_groups[self.node_group_name]
 
+    # Position the new node
+            new_node.location = (420, 420)
+
+    # Handled.
+        except AttributeError:
+            self.report({'WARNING'}, "No active node tree found... Good heavens, fix your life man!")
+            return {'CANCELLED'}
+
+    # If can do, do!
         return {'FINISHED'}
 
+    
 class NODE_PT_my_panel(bpy.types.Panel):
-    bl_label = "Node Groups"
+    bl_idname="NODE_PT_panel"
+    bl_label = "Local Node Groups"
     bl_space_type = 'NODE_EDITOR'
     bl_region_type = 'UI'
     bl_category = "Tool"
 
     def draw(self, context):
-        layout = self.layout
-
-        # Iterate over node groups
+    # Get the node groups
+        node_groups=bpy.data.node_groups
+    
+    # Initialize the UI
+        col = self.layout.column(align=True)
+    
+    # Iterate over node groups
         for ng in bpy.data.node_groups:
-            # Iterate over nodes in the group
-            #for node in ng.nodes:
-                # Create a button for the node
-            op = layout.operator("node.add_node", text=ng.name)
-                
-                # Set the operator's node_name property
-            op.node_name = ng.name
-            print(ng)
-                
-classes=[NODE_PT_my_panel, NODE_OT_add_node]
+    
+    # Create a button for the node group
+            op = col.operator("node_ot.add_node_group", text=ng.name,icon="KEYTYPE_JITTER_VEC")
+    
+    # Set the operator's node_group_name property
+            op.node_group_name = ng.name
+
+    #Registration                
+classes=[NODE_PT_my_panel, NODE_OT_add_node_group]
 
 def register():
     for cls in classes:
@@ -53,98 +94,7 @@ def unregister():
     for cls in classes:
         bpy.utils.unregister_class(cls)
         
+if __name__=="__main__":
+    register()
 
-register()
 
-
-"""old shit
-import bpy
-bl_info = {
-    "name": "RCToolKit",
-    "description": "Starting Toolkit, featuring custom node groups, with minor UI Inclusion",
-    "author": "Ronboprime",
-    "email":"ronboprime@gmail.com",
-    "version": (1, 3),
-    "blender": (4, 0, 0),
-    "location": "Node Editor> Navigation Bar> RCToolKit",
-    "warning": "relax! You'll never make it out, alive!", 
-    "doc_url": "https://www.ronboprime.com/current/rct/docs",
-    "tracker_url": "",
-    "support": "COMMUNITY",
-    "category": "Node",
-}
-
-nodes=["Thouies","Hundoes","Tenths","Smoothing","Half/Double","Third/3x","Quarter/4x","Fifth/5x","Eighth/8x","Puppet","Camera Mockup","Broken At Ranges","Curve To Mesh","QuickInvert","QuickStartIco","QuickStartCube","QuickStartCone","QuickStartCurve","QuickStartTorus","QuickStartGrid"]
-icons=["EMPTY_ARROWS","EMPTY_ARROWS","EMPTY_ARROWS","LINENUMBERS_ON","PREV_KEYFRAME","FF","NEXT_KEYFRAME","REW","SELECT_DIFFERENCE","IPO_BEZIER","SYSTEM","GP_SELECT_POINTS","SEQ_CHROMA_SCOPE","OUTLINER_DATA_CAMERA","PROP_PROJECTED","ORIENTATION_GIMBAL","MOD_PARTICLE_INSTANCE","MESH_CUBE","PROP_OFF","PROP_ON","MESH_GRID"]
-
-data ={node:icon for (node,icon) in zip(nodes,icons)}
-
-node_properties=bpy.context.scene.node_properties
-
-class NodeProperties(bpy.types.PropertyGroup):
-    thingo:bpy.props.StringProperty(
-    name="taco",
-    default="tiddy",
-    description="tuesday"
-    )
-
-class NODE_OT_RCToolKitOperator(bpy.types.Operator):
-    bl_idname='node.rctoperator'
-    bl_label='RCToolKit Operator'
-
-    CNode:bpy.props.StringProperty(
-        name="Custom Node Name",
-        description="For Swapping properties",
-        default="Thouies"
-        )
-    INode:bpy.props.StringProperty(
-        name="Icon for menu",
-        description="also for the swapping",
-        default="EMPTY_ARROWS"
-    )
-    
-    def execute(self,context):
-        bpy.ops.node.add_node(
-        use_transform=True,
-        settings=[{"name":"node_tree",
-               "value":f"bpy.data.node_groups['{self.CNode}']"}]
-               ,type="GeometryNodeGroup")
-        newnode = context.active_node
-        newnode.location.x -= 400
-        return {'FINISHED'}
-
-class NODE_PT_RCToolKitPanel(bpy.types.Panel):
-    bl_idname="NODE_PT_rctpanel"
-    bl_label=" -= RCToolKit =- "
-    bl_space_type='NODE_EDITOR'
-    bl_region_type='UI'
-    bl_category='Tool'
-
-    def draw(self,context):
-        col=self.layout.column(align=True)
-        doodle(col)
-
-def doodle(col):
-   for k,v in data.items():
-     templ8="RCT-"+str(k)
-     NODE_OT_RCToolKitOperator.CNode=templ8
-     NODE_OT_RCToolKitOperator.INode=v
-     col.operator("node.rctoperator",text=f"{templ8}", icon=f"{v}")
-
-classes = [NodeProperties,NODE_OT_RCToolKitOperator,NODE_PT_RCToolKitPanel]
-
-def register():
-    for cls in classes:
-        bpy.utils.register_class(cls)
-        print(str(cls)+" Loaded")
-    bpy.types.Scene.node_properties=bpy.props.PointerProperty(type=NodeProperties)        
-    print("Properties assigned.")    
-    print("RCToolKit Loaded")
-
-def unregister():
-    for cls in classes:
-        bpy.utils.unregister_class(cls)
-    print("Goodbye, cruel world!")    
-
-register()
-"""
